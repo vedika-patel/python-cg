862a7b3526bba95aeccdd37f964882a83a630555
vedika patel <vedika.patel.cg@gmail.com>
2026-09-04 18:40:46 +0530

    add

---
+++ 08_conditional_statement/conditional.py
@@ -0,0 +1,140 @@
+number = int(input("Enter a number: "))
+if number > 10:
+    print("The number is greater than 10.")
+age = int(input("Enter your age: "))
+if age >= 18: 
+    print("adult") 
+positive_number = int(input("Enter a positive number: "))
+if positive_number > 0:
+    print("The number is positive.") 
+mark=int(input("Enter your mark: "))
+if mark >= 40:
+    print("pass") 
+else:  
+    print("fail")
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+positive_number = int(input("Enter a positive number: "))
+if positive_number > 0:
+    print("The number is positive.")
+else:
+    print("The number is not positive.")
+
+age = int(input("Enter your age: ")) 
+if age >=18:
+    print("adult")
+else:
+    print("minor")
+even_number = int(input("Enter an even number: "))
+if even_number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is not even.")
+odd_number = int(input("Enter an odd number: "))
+if odd_number % 2 != 0:
+    print("The number is odd.")
+else:
+    print("The number is not odd.")
+
+marks = int(input("Enter your marks: "))
+if marks >= 40:
+    print("pass")
+else:
+    print("fail") 
+
+a=19
+b=20
+if a>b:
+    print("a is greater than b")
+elif a<b:
+    print("a is less than b")  
+# 11 to 20 question
+mark=int(input("Enter your mark: "))
+if mark >= 90:
+    print("A grade")
+elif mark >= 80:
+    print("B grade")
+elif mark >= 70:
+    print("C grade")
+elif mark >= 40:
+    print("D grade")
+else:
+    print("F grade")
+
+number=int(input("Enter a number: "))
+if number > 0:
+    print("The number is positive.")   
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+number_of_days=int(input("Enter number of days: "))
+if number_of_days == 1:
+    print("Monday")    
+elif number_of_days == 2:
+    print("Tuesday")
+elif number_of_days == 3:
+    print("Wednesday")
+elif number_of_days == 4:
+    print("Thursday")
+elif number_of_days == 5:
+    print("Friday")
+elif number_of_days == 6:
+    print("Saturday")
+elif number_of_days == 7:
+    print("Sunday")
+else:
+    print("Invalid input")
+students_marks=int(input("Enter your marks: "))
+if students_marks >= 90:
+    print("excellent") 
+elif students_marks >= 80:
+    print("good")
+elif students_marks >= 40:
+    print("pass")
+else:
+    print("fail")
+
+if number==1:
+    print("one")
+elif number==2:
+    print("two")
+elif number==3:
+    print("three")
+else:
+    print("other")
+
+if age >= 18 and age <= 60:
+    print("adult")
+
+marks=int(input("Enter your marks: "))
+if marks >= 90:
+    print("excellent")
+    if marks >= 80:
+        print("good")
+        if marks >= 40:
+            print("pass")
+        else:
+            print("fail")
+number=int(input("Enter a number: "))
+if number > 0:
+    print("The number is positive.")
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+else:
+    print("The number is not positive.")
+
+age=int(input("Enter your age: "))
+if age >= 18:   
+    print("adult")
+    if age >= 60:
+        print("senior citizen")
+    else:
+        print("not a senior citizen")
+n        
