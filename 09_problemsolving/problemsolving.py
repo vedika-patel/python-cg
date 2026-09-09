62aa620fe8dc8413f48eecdc7cf00abc6bdb1ab8
vedika patel <vedika.patel.cg@gmail.com>
2026-09-09 23:02:14 +0530

    add the file forloop

--- 09_problemsolving/problemsolving.py
+++ 09_problemsolving/problemsolving.py
@@ -1,162 +1,162 @@
-# levle-1 
-# question:1
-a=int(input("enter the number:"))
-if a>0:
-    print("positive")
-elif a<0:
-    print("Negative") 
-elif a==0:
-    print("Zero")
-# question:2
-b=int(input("enter the number:"))
-if b==0:
-    print("zero")
-elif b>0:
-    if b%2 == 0:
-        print("positive even") 
-    else:
-        print("positive odd") 
-elif b<0:
-    if b%2 == 0:
-        print("negative even") 
-    else:
-        print("negative odd")
-# question:3
-c=int(input("enter the first number:")) 
-d=int(input("enter the second number:")) 
-if c>d:
-    print(c)
-elif c<d:
-    print(d)
-else:
-    print("Both are equal")
-# question:4
-e=int(input("enter the num1:")) 
-f=int(input("enter the num2:")) 
-g=int(input("enter the num3:"))
-if e<=f and e<=g:
-    print(e)  
-elif f<=g and f<=e:
-    print(f) 
-else:
-    print(g)
-#question:5
-h=int(input("enter the num1:")) 
-i=int(input("enter the num2:")) 
-j=int(input("enter the num3:"))
-if h>=i and h>=j:
-    print(h)  
-elif i>=h and i>=j:
-    print(i) 
-else:
-    print(j)
-# question:6
-k=int(input("enter the number:"))
-if k%5 == 0 and k%11 == 0:
-    print("Divisible by both 5 and 11")
-elif k%5 == 0:
-    print("Divisible only by 5")
-elif k%11 == 0:
-    print("Divisible only by 11")
-else:
-    print("Divisible by neither") 
-# question:7
-l=int(input("enter the number:"))
-if l%3 == 0 and l%7 == 0:
-    print("Divisible by both 3 and 7")
-elif l%3 == 0:
-    print("Divisible only by 3")
-elif l%7 == 0:
-    print("Divisible only by 7")
-else:
-    print("Divisible by neither") 
-# question:8
-marks=float(input("enter the  your marks:"))
-if marks<0 or marks>100:
-    print("invalide")
-elif marks>=40:
-    print("pass")
-else:
-    print("fail")
-# qustion:9
-marks=float(input("enter the  your marks:"))
-if marks<0 or marks>100:
-    print("invalid")
-elif marks>=90:
-    print("grade a")
-elif marks>=80:
-    print("grade b")
-elif marks>=70:
-    print("grade c")
-elif marks>=60:
-    print("grade d")
-elif marks>=40:
-    print("grade e")
-else:
-    print("fail")
-# #question:10
-age=int(input("enter your age:"))
-if age<0 or age>120:
-    print("invalid")
-elif age>=18:
-    print("can vote")
-else:
-    print("cannot vote")
-# qustion:11
-year=int(input("enter year:"))
-if (year%400 == 0)or (year%4 ==0 and year%100 !=0):
-    print("leap year")
-else:
-    print("not a leap year")
-# qustion:12
-char=input("enter the charcter:")
-if 'A' <= char <= 'Z': 
-    print("Uppercase alphabet")
-elif 'a'<= char <='z':
-    print("Lowercase alphabet")
-elif '0'<= char <='9':
-    print("Digit")
-else:
-    print("Special character")
-question:13
-#Take a single character input from the user
-char = input("enter the char:")
+# # levle-1 
+# # question:1
+# a=int(input("enter the number:"))
+# if a>0:
+#     print("positive")
+# elif a<0:
+#     print("Negative") 
+# elif a==0:
+#     print("Zero")
+# # question:2
+# b=int(input("enter the number:"))
+# if b==0:
+#     print("zero")
+# elif b>0:
+#     if b%2 == 0:
+#         print("positive even") 
+#     else:
+#         print("positive odd") 
+# elif b<0:
+#     if b%2 == 0:
+#         print("negative even") 
+#     else:
+#         print("negative odd")
+# # question:3
+# c=int(input("enter the first number:")) 
+# d=int(input("enter the second number:")) 
+# if c>d:
+#     print(c)
+# elif c<d:
+#     print(d)
+# else:
+#     print("Both are equal")
+# # question:4
+# e=int(input("enter the num1:")) 
+# f=int(input("enter the num2:")) 
+# g=int(input("enter the num3:"))
+# if e<=f and e<=g:
+#     print(e)  
+# elif f<=g and f<=e:
+#     print(f) 
+# else:
+#     print(g)
+# #question:5
+# h=int(input("enter the num1:")) 
+# i=int(input("enter the num2:")) 
+# j=int(input("enter the num3:"))
+# if h>=i and h>=j:
+#     print(h)  
+# elif i>=h and i>=j:
+#     print(i) 
+# else:
+#     print(j)
+# # question:6
+# k=int(input("enter the number:"))
+# if k%5 == 0 and k%11 == 0:
+#     print("Divisible by both 5 and 11")
+# elif k%5 == 0:
+#     print("Divisible only by 5")
+# elif k%11 == 0:
+#     print("Divisible only by 11")
+# else:
+#     print("Divisible by neither") 
+# # question:7
+# l=int(input("enter the number:"))
+# if l%3 == 0 and l%7 == 0:
+#     print("Divisible by both 3 and 7")
+# elif l%3 == 0:
+#     print("Divisible only by 3")
+# elif l%7 == 0:
+#     print("Divisible only by 7")
+# else:
+#     print("Divisible by neither") 
+# # question:8
+# marks=float(input("enter the  your marks:"))
+# if marks<0 or marks>100:
+#     print("invalide")
+# elif marks>=40:
+#     print("pass")
+# else:
+#     print("fail")
+# # qustion:9
+# marks=float(input("enter the  your marks:"))
+# if marks<0 or marks>100:
+#     print("invalid")
+# elif marks>=90:
+#     print("grade a")
+# elif marks>=80:
+#     print("grade b")
+# elif marks>=70:
+#     print("grade c")
+# elif marks>=60:
+#     print("grade d")
+# elif marks>=40:
+#     print("grade e")
+# else:
+#     print("fail")
+# # #question:10
+# age=int(input("enter your age:"))
+# if age<0 or age>120:
+#     print("invalid")
+# elif age>=18:
+#     print("can vote")
+# else:
+#     print("cannot vote")
+# # qustion:11
+# year=int(input("enter year:"))
+# if (year%400 == 0)or (year%4 ==0 and year%100 !=0):
+#     print("leap year")
+# else:
+#     print("not a leap year")
+# # qustion:12
+# char=input("enter the charcter:")
+# if 'A' <= char <= 'Z': 
+#     print("Uppercase alphabet")
+# elif 'a'<= char <='z':
+#     print("Lowercase alphabet")
+# elif '0'<= char <='9':
+#     print("Digit")
+# else:
+#     print("Special character")
+# question:13
+# #Take a single character input from the user
+# char = input("enter the char:")
 
-# Check if the input is exactly one alphabetic character
-if len(char) == 1 and char.isalpha():
-    # Convert to lowercase to handle both upper and lower case
-    char_lower = char.lower()
+# # Check if the input is exactly one alphabetic character
+# if len(char) == 1 and char.isalpha():
+#     # Convert to lowercase to handle both upper and lower case
+#     char_lower = char.lower()
     
-    # Check if the character is a vowel
-    if char_lower in ['a', 'e', 'i', 'o', 'u']:
-        print("Output: Vowel")
-    else:
-        print("Output: Consonant")
-else:
-    print("valid input")
-question:14
-cost_price=float(input("enter the costprice:"))
-selling_price=float(input("enter the sellingprice:"))
-if selling_price>cost_price:
-    profit=selling_price-cost_price
-    print(f"profi= {profit}")
-elif selling_price<=cost_price:
-    loss=cost_price-selling_price
-    print(f"loss= {loss}")
-else:
-    print("No profit and no loss") 
-cost_price=float(input("enter the costprice:"))
-selling_price=float(input("enter the sellingprice:"))
-if selling_price>cost_price:
-    Profit = selling_price -cost_price
-    profit_pasenger=Profit/cost_price*100
-    print(f"profit ={profit_pasenger} %")
-elif selling_price<cost_price:
-    loss=cost_price-selling_price
-    loss_pasnger=loss/cost_price*100
-    print(f"loss ={loss_pasnger} %")
-else:
-    print("envalid") 
+#     # Check if the character is a vowel
+#     if char_lower in ['a', 'e', 'i', 'o', 'u']:
+#         print("Output: Vowel")
+#     else:
+#         print("Output: Consonant")
+# else:
+#     print("valid input")
+# question:14
+# cost_price=float(input("enter the costprice:"))
+# selling_price=float(input("enter the sellingprice:"))
+# if selling_price>cost_price:
+#     profit=selling_price-cost_price
+#     print(f"profi= {profit}")
+# elif selling_price<=cost_price:
+#     loss=cost_price-selling_price
+#     print(f"loss= {loss}")
+# else:
+#     print("No profit and no loss") 
+# cost_price=float(input("enter the costprice:"))
+# selling_price=float(input("enter the sellingprice:"))
+# if selling_price>cost_price:
+#     Profit = selling_price -cost_price
+#     profit_pasenger=Profit/cost_price*100
+#     print(f"profit ={profit_pasenger} %")
+# elif selling_price<cost_price:
+#     loss=cost_price-selling_price
+#     loss_pasnger=loss/cost_price*100
+#     print(f"loss ={loss_pasnger} %")
+# else:
+#     print("envalid") 
 # question:16
   
 
---
+++ 10_forloop/for.py
@@ -0,0 +1,12 @@
+# Q-1
+for i in range(1,6):
+    print("Hello")
+# Q-2
+for i in range(0,10):
+    print(i,end=" ")
+#Q-3
+for i in range(1,11):
+    print(i)
+#Q-4
+for i in range(10,0,-1):
+    print(i)
